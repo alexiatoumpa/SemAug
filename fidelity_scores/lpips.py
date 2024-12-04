0977f1fa2255ee7b2e3998d78af487f8ce598fc7
Alexia Toumpa <alexiatoumpa@gmail.com>
2024-12-04 15:02:53 +0000

    added normalizing function for the lpips score

--- fidelity_scores/lpips.py
+++ fidelity_scores/lpips.py
@@ -5,22 +5,38 @@ RGB images.
 
 from PIL import Image
 import lpips
-import torch
+import numpy as np
+
+def normalize_image(image):
+    image = image.astype(np.float32)
+    # normalize pixel values to [0,1]
+    image /= 255.0
+    # scale values to [-1,1]
+    image = image * 2 - 1
+    return image
+
 
 def calculate_lpips_score(image1path, image2path):
     # load images
     image1 = Image.open(image1path).convert("RGB")
     image2 = Image.open(image2path).convert("RGB")
 
+    image1 = np.array(image1)
+    image2 = np.array(image2)
+
+    # image values should be normalized to [-1, 1]
+    image1 = normalize_image(image1)
+    image2 = normalize_image(image2)
+
     # initialize the lpips function
     lpips_func = lpips.LPIPS(net='alex') # net: 'alex', 'vgg', 'squeeze'
 
-    # image values should be normalized to [-1, 1]
-    
     # compute LPIPS score
     lpips_score = lpips_func(image1, image2)
     print(f"LPIPS score: {lpips_score}")
 
-    return lpips_score
+    return lpips_score_score
+
+
 
 

b57b80ed1ecd5e97188746a5719b185a97331360
Alexia Toumpa <alexiatoumpa@gmail.com>
2025-09-09 22:37:42 +0100

    reading config file for some hyperparameters

--- run_semaug.py
+++ run_semaug.py
@@ -1,5 +1,5 @@
 from augmentation.InpaintingDifussionModel import Inpainting
-from process import augment_images, augment_cifar_images
+from process import augment_images
 from nlp.Caption_Enrichement_NLP import *
 
 import numpy as np
@@ -10,50 +10,42 @@ from utils.datasets import (load_cifar10_dataset, load_custom_dataset,
                             load_test_imagepaths, load_imagenet_dataset
 )
 import csv
-import os
+# import os
 import argparse
 from datetime import datetime
-import pdb
-
+# import pdb
+import yaml
 
 
 __version__ = 0.2
+
+
 def parse_arguments():
     """
     Parse command line argument and construct the DNN
     :return: a dictionary comprising the command-line arguments
     """
     text = 'Data Augmentation pipline'
-
-    # initiate the parser
     parser = argparse.ArgumentParser(description=text)
-
     # new command-line arguments
     parser.add_argument("-V", "--version",  help="show program version",
                         action="version", version="DeepFault %f" % __version__)
-    parser.add_argument("-M", "--methodology", help="semantic augmentation methodology", 
-                        choices=['Inpainting','Imagic'])
     parser.add_argument("-DS", "--dataset", default="cifar10", help="The dataset to be used (imagenet, cifar10, or leaf).", 
                         choices=["imagenet","cifar10","leaf"])
-    parser.add_argument("-PG", "--path2graph", help="Path to the mask prediction model graph file.", 
-                        type=str)
-    parser.add_argument("-PW", "--path2weights", help="Path to the mask prediction model weights file.", 
-                        type=str)
-    # parser.add_argument("-Me", "--measure", help="the approach to be employed \
-    #                         to measure similarity", choices=['SSIM', 'FID'])
-
-    parser.add_argument("-Cap", "--Augmented_caption", help="the image caption")
-    parser.add_argument("-K", "--iteration", help="nbre of iteration for augmenting an image.", 
-                        type=int)
     parser.add_argument("-SS", "--seed_size", default=2, help="size of initial set of seed images.", 
                         type=int)
-    parser.add_argument("-LOG", "--logfile", default='DataAugment.log', help="path to log file")
-
-    parser.add_argument("-FS", "--fidelity", action='store_true', help="Bollean argument for computing the fidelity scores.")
-
+  
     args = parser.parse_args()
 
     return vars(args)
+
+
+def load_config(config_path='config.yaml'):
+    with open("config.yaml", "r") as f:
+        config = yaml.safe_load(f)
+
+    config['iteration'] = int(config['iteration'])
+    return config
 
 
 # def save_image(Aug_img, base_file, approach):
@@ -70,43 +62,46 @@ if __name__ == "__main__":
 
     args = parse_arguments()
     print(args)
-    approach = args['methodology'] if args['methodology'] else 'Inpainting'
 
-    # nlp = spacy.load("en_core_web_sm")
-    # taxonomy = ['smiling', 'waving', 'talking', 'sleeping', 'siting', 'laughting', 
-    #             'jumping', 'wearing a mask']
-    # measure = args['measure'] if not args['measure'] == None else 'SSIM'
-    # caption = args['intial_caption'] if not args['intial_caption'] == None else 'a person'
-    Augmented_caption = args['Augmented_caption'] if not args['Augmented_caption'] == None \
-        else 'a person'
-    seed_size = args['seed_size']
     dataset = args['dataset']
-    path2graph=args['path2graph'] if 'path2graph' in args else "/home/alexiatoumpa/dev/Github/SemAug/models/frozen_inference_graph.pb"
-    path2weights=args['path2weights'] if 'path2weights' in args else "/home/alexiatoumpa/dev/Github/SemAug/models/mask_rcnn_inception_v2_coco_2018_01_28.pbtxt"
-    # datatype = args['datatype'] if not args['datatype'] == None else 'cifar'
-    logfile_name = args['logfile']
+    seed_size = args['seed_size']
+
+    config = load_config(config_path='config.yaml')
+
+    approach = config['methodology'] if config['methodology'] else 'Inpainting'
+    approach = 'Inpainting'
+
+    path2graph = config['path2graph']
+    path2weights = config['path2weights']
+
+    logfile_name = config['logfile']
     logfile = open(logfile_name, 'a')
     extension='.csv'
 
-    iteration="_11_"
+    fidelity_analysis = config['fidelity']
+
+    iteration = "_" +str(config['iteration'])+ "_"
+
+    augmented_caption = config['augmented_caption'] if not config['augmented_caption'] == None \
+        else 'a person'
+    
+    device = config['device'] if config['device'] else "cpu"
+    print("device:", device)
+
     # Format as DATE - REGION - REPORT TYPE
     start_time = datetime.now()
     results=[]
-    fidelity_analysis = args['fidelity']
-    # line = [str(id), SSIM_inpaint, SSIM_E, SSIM_N, FID_inpainting, FID_Erase, FID_Noise, clip_score.item()]
-    # results.append(line)
-    # images.append({"id": str(id), "caption": Initial_caption, "Aug_caption": aug_caption_Category, "original": ini_path,
-    #                "Inpainting": inpaint_path, "Erase": erase_path, "Noise": noise_path})
 
     entete_results=['img_id', 'caption', 'augmented_caption','method', 'MSE', 
-                    'PSNR','FID', 'SSIM', 'LPIPS_alex', 'LPIPS_vgg', 'LPIPS_squeeze', 'VIF']
+                    'PSNR', 'FID', 'SSIM', 'LPIPS_alex', 'LPIPS_vgg', 
+                    'LPIPS_squeeze', 'VIF']
 
     # device = torch.device("cpu")
     # entete_imgs=['id', 'caption', 'augmented_caption', 'original_img', 'inpainting', 
     #              'erase', 'noise', 'label', 'augmentation_category']
-    entete_imgs=['id', 'caption', 'augmented_caption', 'method', 'original_img', 'augmented_img', 
-                 'label', 'augmentation_category']
-    approach = 'Inpainting'
+    entete_imgs=['id', 'caption', 'augmented_caption', 'method', 'original_img', 
+                 'augmented_img', 'label', 'augmentation_category']
+    
 
     date = datetime.strftime(datetime.now(), '%Y-%m-%d')
     info = {f'{date} - {approach} - {dataset} - {extension}'}
@@ -117,31 +112,28 @@ if __name__ == "__main__":
     file_images=f'{date}-{approach}-{dataset}-{a}-{iteration}-{extension}'
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     # with torch.cuda.amp.autocast(True):
-    #####dataset and input
+    ##### dataset and input
     start_time = datetime.now()
     logfile_name = './results/DataAugment.log'
     logfile = open(logfile_name, 'a')
 
     print(dataset,"loading... ... ...")
+
     if dataset=='cifar10':
-        categories = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 
-                    'horse', 'ship', 'truck']
+        categories = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog',
+                      'frog', 'horse', 'ship', 'truck']
         data_directory_path = "./data/cifar/Augmented/"
 
         # Load dataset
-        # (x_train, y_train), (x_test, y_test) = cifar10.load_data()
         (x_train, y_train), (x_test, y_test) = load_cifar10_dataset()
         print("x_test shape:", x_test.shape)
         print("y_test shape:", y_test.shape)
         print(type(x_test))
         print(type(y_test))
-        # Create augmented data
-        # images, scores = augment_cifar_images(x_test, y_test, dataset=dataset,
-        #                                 seed_size=seed_size, data_directory_path=data_directory_path, 
-        #                                 categories=categories, path2graph=path2graph, path2weights=path2weights)
+
     elif dataset=='imagenet':
-        categories = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 
-                    'horse', 'ship', 'truck']
+        categories = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog',
+                      'frog', 'horse', 'ship', 'truck']
         data_directory_path = "./data/imagenet/Augmented/"
         # Load dataset
         dataset_path = '/home/alexiatoumpa/data/ImageNet/ILSVRC/Data/DET/test/'
@@ -175,7 +167,7 @@ if __name__ == "__main__":
                                     seed_size=seed_size, data_directory_path=data_directory_path, 
                                     categories=categories, augmentation_type=['Inpainting'],
                                     path2graph=path2graph, path2weights=path2weights, 
-                                    fidelity_analysis=fidelity_analysis)
+                                    fidelity_analysis=fidelity_analysis, device=device)
 
     print("size of augmented data set:", len(images))
     if len(scores)!=0:
